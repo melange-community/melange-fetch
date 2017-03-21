affc1d137fd521eab5b99711f79237faa3def114
glennsl <glenn@slotte.net>
2017-03-21 15:16:51 +0100

    fixed formatting and readded comments

--- src/bs_fetch.ml
+++ src/bs_fetch.ml
@@ -7,12 +7,15 @@ type headersInit
 type response
 type request
 type requestInit
-type arrayBuffer
-type blob
-type bufferSource
-type formData
-type readableStream
-type urlSearchParams
+
+(* external *)
+type arrayBuffer (* TypedArray *)
+type blob (* FileAPI *)
+type bufferSource (* Web IDL, either an arrayBuffer or arrayBufferView *)
+type formData (* XMLHttpRequest *)
+type readableStream (* Streams *)
+type urlSearchParams (* URL *)
+
 type requestMethod =
   | Get
   | Head
@@ -24,7 +27,7 @@ type requestMethod =
   | Trace
   | Patch
   | Other of string
-let encodeRequestMethod =
+let encodeRequestMethod = (* internal *)
   function
   | Get  -> "GET"
   | Head  -> "HEAD"
@@ -35,8 +38,8 @@ let encodeRequestMethod =
   | Options  -> "OPTIONS"
   | Trace  -> "TRACE"
   | Patch  -> "PATCH"
-  | ((Other (method_))[@explicit_arity ]) -> method_
-let decodeRequestMethod =
+  | Other method_ -> method_
+let decodeRequestMethod = (* internal *)
   function
   | "GET" -> Get
   | "HEAD" -> Head
@@ -47,7 +50,8 @@ let decodeRequestMethod =
   | "OPTIONS" -> Options
   | "TRACE" -> Trace
   | "PATCH" -> Patch
-  | method_ -> ((Other (method_))[@explicit_arity ])
+  | method_ -> Other method_
+
 type referrerPolicy =
   | None
   | NoReferrer
@@ -58,7 +62,7 @@ type referrerPolicy =
   | OriginWhenCrossOrigin
   | StrictOriginWhenCrossOrigin
   | UnsafeUrl
-let encodeReferrerPolicy =
+let encodeReferrerPolicy = (* internal *)
   function
   | None  -> ""
   | NoReferrer  -> "no-referrer"
@@ -69,7 +73,7 @@ let encodeReferrerPolicy =
   | OriginWhenCrossOrigin  -> "origin-when-cross-origin"
   | StrictOriginWhenCrossOrigin  -> "strict-origin-when-cross-origin"
   | UnsafeUrl  -> "unsafe-url"
-let decodeReferrerPolicy =
+let decodeReferrerPolicy = (* internal *)
   function
   | "" -> None
   | "no-referrer" -> NoReferrer
@@ -80,10 +84,10 @@ let decodeReferrerPolicy =
   | "origin-when-cross-origin" -> OriginWhenCrossOrigin
   | "strict-origin-when-cross-origin" -> StrictOriginWhenCrossOrigin
   | "unsafe-url" -> UnsafeUrl
-  | e ->
-      raise ((Failure (("Unknown referrerPolicy: " ^ e)))[@explicit_arity ])
+  | e -> raise (Failure ("Unknown referrerPolicy: " ^ e))
+
 type requestType =
-  | None
+  | None (* default? unknown? just empty string in spec *)
   | Audio
   | Font
   | Image
@@ -91,7 +95,7 @@ type requestType =
   | Style
   | Track
   | Video
-let decodeRequestType =
+let decodeRequestType = (* internal *)
   function
   | "" -> None
   | "audio" -> Audio
@@ -101,9 +105,10 @@ let decodeRequestType =
   | "style" -> Style
   | "track" -> Track
   | "video" -> Video
-  | e -> raise ((Failure (("Unknown requestType: " ^ e)))[@explicit_arity ])
+  | e -> raise (Failure ("Unknown requestType: " ^ e))
+
 type requestDestination =
-  | None
+  | None (* default? unknown? just empty string in spec *)
   | Document
   | Embed
   | Font
@@ -118,7 +123,7 @@ type requestDestination =
   | Style
   | Worker
   | Xslt
-let decodeRequestDestination =
+let decodeRequestDestination = (* internal *)
   function
   | "" -> None
   | "document" -> Document
@@ -135,44 +140,43 @@ let decodeRequestDestination =
   | "style" -> Style
   | "worker" -> Worker
   | "xslt" -> Xslt
-  | e ->
-      raise
-        ((Failure (("Unknown requestDestination: " ^ e)))[@explicit_arity ])
+  | e -> raise (Failure ("Unknown requestDestination: " ^ e))
+
 type requestMode =
   | Navigate
   | SameOrigin
   | NoCORS
   | CORS
-let encodeRequestMode =
+let encodeRequestMode = (* internal *)
   function
   | Navigate  -> "navigate"
   | SameOrigin  -> "same-origin"
   | NoCORS  -> "no-cors"
   | CORS  -> "cors"
-let decodeRequestMode =
+let decodeRequestMode = (* internaø *)
   function
   | "navigate" -> Navigate
   | "same-origin" -> SameOrigin
   | "no-cors" -> NoCORS
   | "cors" -> CORS
-  | e -> raise ((Failure (("Unknown requestMode: " ^ e)))[@explicit_arity ])
+  | e -> raise (Failure ("Unknown requestMode: " ^ e))
+
 type requestCredentials =
   | Omit
   | SameOrigin
   | Include
-let encodeRequestCredentials =
+let encodeRequestCredentials = (* internal *)
   function
   | Omit  -> "omit"
   | SameOrigin  -> "same-origin"
   | Include  -> "include"
-let decodeRequestCredentials =
+let decodeRequestCredentials = (* internal *)
   function
   | "omit" -> Omit
   | "same-origin" -> SameOrigin
   | "imclude" -> Include
-  | e ->
-      raise
-        ((Failure (("Unknown requestCredentials: " ^ e)))[@explicit_arity ])
+  | e -> raise (Failure ("Unknown requestCredentials: " ^ e))
+
 type requestCache =
   | Default
   | NoStore
@@ -180,7 +184,7 @@ type requestCache =
   | NoCache
   | ForceCache
   | OnlyIfCached
-let encodeRequestCache =
+let encodeRequestCache = (* internal *)
   function
   | Default  -> "default"
   | NoStore  -> "no-store"
@@ -188,7 +192,7 @@ let encodeRequestCache =
   | NoCache  -> "no-cache"
   | ForceCache  -> "force-cache"
   | OnlyIfCached  -> "only-if-cached"
-let decodeRequestCache =
+let decodeRequestCache = (* internal *)
   function
   | "default" -> Default
   | "no-store" -> NoStore
@@ -196,158 +200,165 @@ let decodeRequestCache =
   | "no-cache" -> NoCache
   | "force-cache" -> ForceCache
   | "only-if-cached" -> OnlyIfCached
-  | e -> raise ((Failure (("Unknown requestCache: " ^ e)))[@explicit_arity ])
+  | e -> raise (Failure ("Unknown requestCache: " ^ e))
+
 type requestRedirect =
   | Follow
   | Error
   | Manual
-let encodeRequestRedirect =
-  function | Follow  -> "follow" | Error  -> "error" | Manual  -> "manual"
-let decodeRequestRedirect =
+let encodeRequestRedirect = (* internal *)
+  function
+  | Follow  -> "follow"
+  | Error  -> "error"
+  | Manual  -> "manual"
+let decodeRequestRedirect = (* internal *)
   function
   | "follow" -> Follow
   | "error" -> Error
   | "manual" -> Manual
-  | e ->
-      raise ((Failure (("Unknown requestRedirect: " ^ e)))[@explicit_arity ])
-module type Type  = sig type t end
-module HeadersInit =
-  struct
-    type t = headersInit
-    external make : < .. > Js.t -> t = "%identity"
-    external makeWithArray : (string* string) array -> t = "%identity"
+  | e -> raise (Failure ("Unknown requestRedirect: " ^ e))
+
+module HeadersInit = struct
+  type t = headersInit
+  external make : < .. > Js.t -> t = "%identity"
+  external makeWithArray : (string* string) array -> t = "%identity"
+end
+
+module Headers = struct
+  type t = headers
+  external make : t = "Headers" [@@bs.new]
+  external makeWithInit : headersInit -> t = "Headers" [@@bs.new]
+  external append : string -> string = "" [@@bs.send.pipe: t]
+  external delete : string = "" [@@bs.send.pipe: t]
+  (* entries *) (* very experimental *)
+  external get : string -> string option = "" [@@bs.send.pipe: t] [@@bs.return {null_to_opt}]
+  external has : string -> bool = "" [@@bs.send.pipe: t]
+  (* keys *) (* very experimental *)
+  external set : string -> string -> unit = "" [@@bs.send.pipe: t]
+  (* values *) (* very experimental *)
+end
+
+module BodyInit = struct
+  type t = bodyInit
+  external make : string -> t = "%identity"
+  external makeWithBlob : blob -> t = "%identity"
+  external makeWithBufferSource : bufferSource -> t = "%identity"
+  external makeWithFormData : formData -> t = "%identity"
+  external makeWithUrlSearchParams : urlSearchParams -> t = "%identity"
+end
+
+module Body = struct
+  module Impl(T:sig type t end) = struct
+    external body : T.t -> readableStream = "" [@@bs.get]
+    external bodyUsed : T.t -> bool = "" [@@bs.get]
+    external arrayBuffer : (arrayBuffer, unit) promise = "" [@@bs.send.pipe: T.t]
+    external blob : (blob, unit) promise = "" [@@bs.send.pipe: T.t]
+    external formData : (formData, unit) promise = "" [@@bs.send.pipe: T.t]
+    external json : (< .. > Js.t, unit) promise = "" [@@bs.send.pipe: T.t]
+    external text : (string,unit) promise = "" [@@bs.send.pipe: T.t]
   end
-module Headers =
-  struct
-    type t = headers
-    external make : t = "Headers"[@@bs.new ]
-    external makeWithInit : headersInit -> t = "Headers"[@@bs.new ]
-    external append : string -> string = ""[@@bs.send.pipe :t]
-    external delete : string = ""[@@bs.send.pipe :t]
-    external get : string -> string option = ""[@@bs.send.pipe :t][@@bs.return
-                                                                    {
-                                                                    null_to_opt
-                                                                    }]
-    external has : string -> bool = ""[@@bs.send.pipe :t]
-    external set : string -> string -> unit = ""[@@bs.send.pipe :t]
-  end
-module BodyInit =
-  struct
-    type t = bodyInit
-    external make : string -> t = "%identity"
-    external makeWithBlob : blob -> t = "%identity"
-    external makeWithBufferSource : bufferSource -> t = "%identity"
-    external makeWithFormData : formData -> t = "%identity"
-    external makeWithUrlSearchParams : urlSearchParams -> t = "%identity"
-  end
-module Body =
-  struct
-    module Impl(T:sig type t end) =
-      struct
-        external body : T.t -> readableStream = ""[@@bs.get ]
-        external bodyUsed : T.t -> bool = ""[@@bs.get ]
-        external arrayBuffer : (arrayBuffer,unit) promise = ""[@@bs.send.pipe
-                                                                :T.t]
-        external blob : (blob,unit) promise = ""[@@bs.send.pipe :T.t]
-        external formData : (formData,unit) promise = ""[@@bs.send.pipe :T.t]
-        external json : (< .. > Js.t,unit) promise = ""[@@bs.send.pipe :T.t]
-        external text : (string,unit) promise = ""[@@bs.send.pipe :T.t]
-      end
-    include Impl(struct type t = body end)
-  end
-module RequestInit =
-  struct
-    type t = requestInit
-    let map f =
-      function | ((Some (v))[@explicit_arity ]) -> Some (f v) | None  -> None
-    external make :
-      ?method_:string ->
-        ?headers:headersInit ->
-          ?body:bodyInit ->
-            ?referrer:string ->
-              ?referrerPolicy:string ->
-                ?mode:string ->
-                  ?credentials:string ->
-                    ?cache:string ->
-                      ?redirect:string ->
-                        ?integrity:string ->
-                          ?keepalive:Js.boolean -> unit -> requestInit = ""
-    [@@bs.obj ]
-    let make ?method_:(method_ : requestMethod option) 
-      ?headers:(headers : headersInit option)  ?body:(body : bodyInit option)
-       ?referrer:(referrer : string option) 
-      ?referrerPolicy:((referrerPolicy : referrerPolicy)= None) 
-      ?mode:(mode : requestMode option) 
-      ?credentials:(credentials : requestCredentials option) 
-      ?cache:(cache : requestCache option) 
-      ?redirect:(redirect : requestRedirect option) 
-      ?integrity:((integrity : string)= "") 
-      ?keepalive:(keepalive : bool option)  =
-      make ?method_:(map encodeRequestMethod method_) ?headers ?body
-        ?referrer ~referrerPolicy:(encodeReferrerPolicy referrerPolicy)
-        ?mode:(map encodeRequestMode mode)
-        ?credentials:(map encodeRequestCredentials credentials)
-        ?cache:(map encodeRequestCache cache)
-        ?redirect:(map encodeRequestRedirect redirect) ~integrity
-        ?keepalive:(map Js.Boolean.to_js_boolean keepalive)
-  end
-module Request =
-  struct
-    type t = request
-    external make : string -> t = "Request"[@@bs.new ]
-    external makeWithInit : string -> requestInit -> t = "Request"[@@bs.new ]
-    external makeWithRequest : t -> t = "Request"[@@bs.new ]
-    external makeWithRequestInit : t -> requestInit -> t = "Request"[@@bs.new
-                                                                    ]
-    external method_ : t -> string = "method"[@@bs.get ]
-    let method_: t -> requestMethod =
-      fun self  -> decodeRequestMethod (method_ self)
-    external url : t -> string = ""[@@bs.get ]
-    external headers : t -> headers = ""[@@bs.get ]
-    external type_ : t -> string = "type"[@@bs.get ]
-    let type_: t -> requestType = fun self  -> decodeRequestType (type_ self)
-    external destination : t -> string = ""[@@bs.get ]
-    let destination: t -> requestDestination =
-      fun self  -> decodeRequestDestination (destination self)
-    external referrer : t -> string = ""[@@bs.get ]
-    external referrerPolicy : t -> string = ""[@@bs.get ]
-    let referrerPolicy: t -> referrerPolicy =
-      fun self  -> decodeReferrerPolicy (referrerPolicy self)
-    external mode : t -> string = ""[@@bs.get ]
-    let mode: t -> requestMode = fun self  -> decodeRequestMode (mode self)
-    external credentials : t -> string = ""[@@bs.get ]
-    let credentials: t -> requestCredentials =
-      fun self  -> decodeRequestCredentials (credentials self)
-    external cache : t -> string = ""[@@bs.get ]
-    let cache: t -> requestCache =
-      fun self  -> decodeRequestCache (cache self)
-    external redirect : t -> string = ""[@@bs.get ]
-    let redirect: t -> requestRedirect =
-      fun self  -> decodeRequestRedirect (redirect self)
-    external integrity : t -> string = ""[@@bs.get ]
-    external keepalive : t -> bool = ""[@@bs.get ]
-    include Body.Impl(struct type t = body end)
-  end
-module Response =
-  struct
-    type t = response
-    external error : unit -> t = ""[@@bs.val ]
-    external redirect : string -> t = ""[@@bs.val ]
-    external redirectWithStatus : string -> int -> t = "redirect"[@@bs.val ]
-    external headers : t -> headers = ""[@@bs.get ]
-    external ok : t -> bool = ""[@@bs.get ]
-    external redirected : t -> bool = ""[@@bs.get ]
-    external status : t -> int = ""[@@bs.get ]
-    external statusText : t -> string = ""[@@bs.get ]
-    external type_ : t -> string = "type"[@@bs.get ]
-    external url : t -> string = ""[@@bs.get ]
-    external clone : t = ""[@@bs.send.pipe :t]
-    include Body.Impl(struct type t = response end)
-  end
-external fetch : string -> (response,unit) promise = ""[@@bs.val ]
-external fetchWithInit :
-  string -> requestInit -> (response,unit) promise = "fetch"[@@bs.val ]
-external fetchWithRequest : request -> (response,unit) promise = "fetch"
-[@@bs.val ]
-external fetchWithRequestInit :
-  request -> requestInit -> (response,unit) promise = "fetch"[@@bs.val ]
+
+  include Impl(struct type t = body end)
+end
+
+module RequestInit = struct
+  type t = requestInit
+
+  let map f = function (* internal *)
+  | Some v -> Some (f v)
+  | None  -> None
+
+  external make :
+    ?method_:string ->
+    ?headers:headersInit ->
+    ?body:bodyInit ->
+    ?referrer:string ->
+    ?referrerPolicy:string ->
+    ?mode:string ->
+    ?credentials:string ->
+    ?cache:string ->
+    ?redirect:string ->
+    ?integrity:string ->
+    ?keepalive:Js.boolean ->
+    unit -> requestInit = "" [@@bs.obj]
+  let make
+    ?method_:(method_: requestMethod option) 
+    ?headers:(headers: headersInit option)
+    ?body:(body: bodyInit option)
+    ?referrer:(referrer: string option) 
+    ?referrerPolicy:(referrerPolicy: referrerPolicy = None) 
+    ?mode:(mode: requestMode option) 
+    ?credentials:(credentials: requestCredentials option) 
+    ?cache:(cache: requestCache option) 
+    ?redirect:(redirect: requestRedirect option) 
+    ?integrity:(integrity: string = "") 
+    ?keepalive:(keepalive: bool option)
+    = make
+        ?method_: (map encodeRequestMethod method_)
+        ?headers
+        ?body
+        ?referrer
+        ~referrerPolicy: (encodeReferrerPolicy referrerPolicy)
+        ?mode: (map encodeRequestMode mode)
+        ?credentials: (map encodeRequestCredentials credentials)
+        ?cache: (map encodeRequestCache cache)
+        ?redirect: (map encodeRequestRedirect redirect)
+        ~integrity
+        ?keepalive: (map Js.Boolean.to_js_boolean keepalive)
+end
+
+module Request = struct
+  type t = request
+
+  external make : string -> t = "Request" [@@bs.new]
+  external makeWithInit : string -> requestInit -> t = "Request" [@@bs.new]
+  external makeWithRequest : t -> t = "Request" [@@bs.new]
+  external makeWithRequestInit : t -> requestInit -> t = "Request" [@@bs.new]
+
+  external method_ : t -> string = "method"[@@bs.get]
+  let method_: t -> requestMethod = fun self -> decodeRequestMethod (method_ self)
+  external url : t -> string = ""[@@bs.get]
+  external headers : t -> headers = ""[@@bs.get]
+  external type_ : t -> string = "type"[@@bs.get]
+  let type_: t -> requestType = fun self -> decodeRequestType (type_ self)
+  external destination : t -> string = ""[@@bs.get]
+  let destination: t -> requestDestination = fun self -> decodeRequestDestination (destination self)
+  external referrer : t -> string = ""[@@bs.get]
+  external referrerPolicy : t -> string = ""[@@bs.get]
+  let referrerPolicy: t -> referrerPolicy = fun self -> decodeReferrerPolicy (referrerPolicy self)
+  external mode : t -> string = ""[@@bs.get]
+  let mode: t -> requestMode = fun self  -> decodeRequestMode (mode self)
+  external credentials : t -> string = ""[@@bs.get]
+  let credentials: t -> requestCredentials = fun self -> decodeRequestCredentials (credentials self)
+  external cache : t -> string = ""[@@bs.get]
+  let cache: t -> requestCache = fun self -> decodeRequestCache (cache self)
+  external redirect : t -> string = ""[@@bs.get]
+  let redirect: t -> requestRedirect = fun self -> decodeRequestRedirect (redirect self)
+  external integrity : t -> string = ""[@@bs.get]
+  external keepalive : t -> bool = ""[@@bs.get]
+
+  include Body.Impl(struct type t = body end)
+end
+
+module Response = struct
+  type t = response
+
+  external error : unit -> t = "" [@@bs.val]
+  external redirect : string -> t = "" [@@bs.val]
+  external redirectWithStatus : string -> int (* enum-ish *) -> t = "redirect" [@@bs.val]
+  external headers : t -> headers = "" [@@bs.get]
+  external ok : t -> bool = "" [@@bs.get]
+  external redirected : t -> bool = "" [@@bs.get]
+  external status : t -> int = "" [@@bs.get]
+  external statusText : t -> string = "" [@@bs.get]
+  external type_ : t -> string = "type" [@@bs.get]
+  external url : t -> string = "" [@@bs.get]
+  external clone : t = "" [@@bs.send.pipe: t]
+
+  include Body.Impl(struct type t = response end)
+end
+
+external fetch : string -> (response, unit) promise = "" [@@bs.val]
+external fetchWithInit : string -> requestInit -> (response, unit) promise = "fetch" [@@bs.val]
+external fetchWithRequest : request -> (response, unit) promise = "fetch" [@@bs.val]
+external fetchWithRequestInit : request -> requestInit -> (response, unit) promise = "fetch" [@@bs.val]
