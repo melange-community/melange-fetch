1c7c58b49744f960e5a81818cfd619d3b7081430
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-09-14 14:30:55 -0700

    feat: migrate to melange v2 (next) (#42)

--- src/iterator.mli
+++ src/iterator.mli
@@ -6,14 +6,16 @@
 module Next : sig
   type 'a t
 
-  external done_ : _ t -> bool option = "done" [@@bs.get]
-  external value : 'a t -> 'a option = "value" [@@bs.get] [@@bs.return nullable]
+  external done_ : _ t -> bool option = "done" [@@mel.get]
+
+  external value : 'a t -> 'a option = "value"
+  [@@mel.get] [@@mel.return nullable]
 end
 
 type 'a t
 
-val forEach : f:(('a -> unit)[@bs]) -> 'a t -> unit
+val forEach : f:(('a -> unit)[@u]) -> 'a t -> unit
 (** [forEach ~f iterator] runs [f] on each item returned by the [iterator]. This
     is not defined by the platform but a convenience function. *)
 
-external next : 'a t -> 'a Next.t = "next" [@@bs.send]
+external next : 'a t -> 'a Next.t = "next" [@@mel.send]
